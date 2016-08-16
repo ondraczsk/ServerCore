d7bafa83c5e8ed736cdd286e61738e79135eac45
GamakCZ <gamak.mcpe@gmail.com>
2016-08-16 22:40:20 +0200

    Update Core.php

--- src/GamakCZ/Core.php
+++ src/GamakCZ/Core.php
@@ -12,9 +12,13 @@ use pocketmine\plugin\PluginBase as P;
 use pocketmine\event\Listener as L;
 
 use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config
+
+use pocketmine\event\player\PlayerChatEvent;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+
 
 class Core extends P implements L {
   public $prefix = C::DARK_BLUE . "[" . C::YELLOW . "Server" . C::GOLD . "Core" . C::DARK_BLUE . "]";
@@ -24,12 +28,22 @@ class Core extends P implements L {
     $this->getLogger->info->(" enabled");
   }
   
-  public function onChat() {
+  public function __construct(ServerCore, $plugin) {
     
   }
-  public function onCommand() {
+  public function onChat(PlayerChatEvent $pche) {
+    $pl = $pche->getPlayer();
+    $msg = $event->getMessage();
       
-      
+      $event->setFormat(C::DARK_GREEN . $pl . C::DARK_GRAY . ": " . C:GRAY . $msg);
+    }
+    
+  public function onCommand(CommandSender $s, Command $cmd, $lavel, Array $args) {
+    if($sender instanceof Player) {
+      return true;
+    }
+    switch (strtolower($cmd->getName())) {
+      case "":
     }
   }
 }
