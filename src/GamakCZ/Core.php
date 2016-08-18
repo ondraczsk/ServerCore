966f14eb4d18efe1f5e58e062c438a58bf0b456f
kaliiks <kaliik1@seznam.cz>
2016-08-18 10:28:32 +0300

    Updated Core.php

--- src/GamakCZ/Core.php
+++ src/GamakCZ/Core.php
@@ -43,7 +43,8 @@ class Core extends P implements L {
       return true;
     }
     switch (strtolower($cmd->getName())) {
-      case "":
+      case "sc":
+      $sender->sendMessage(this->$prefix. ("test");
     }
   }
 }
